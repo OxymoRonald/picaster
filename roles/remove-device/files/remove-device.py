89195982573e8f3f3983eb8170e6b9fba0a363ce
OxymoRonald <rwiersema@gmail.com>
2022-06-01 18:16:04 +0200

    Added remove-device

--- roles/remove-device/files/remove-device.py
+++ roles/remove-device/files/remove-device.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # Set variables
 inventory_directory = "/home/picaster/picaster/"
 inventory_file = inventory_directory + "inventory.yml"
-inventory_group = sys.argv[1]
+inventory_device = sys.argv[1]
 
 # Open inventory file as read only.
 # print(">> Import inventory file read only <<")
@@ -21,8 +21,8 @@ inventory_dict = yaml.load(inventory, Loader=yaml.FullLoader)
 inventory.close()
 
 #Pretty print the inventory
-print(">> Pretty print inventory <<")
-print(yaml.dump(inventory_dict, default_flow_style=False))
+# print(">> Pretty print inventory <<")
+# print(yaml.dump(inventory_dict, default_flow_style=False))
 
 # Backup the inventory
 # Get current time for backup
@@ -32,15 +32,34 @@ backupfile.write("---\n")
 backupfile.write(yaml.dump(inventory_dict, default_flow_style=False)) 
 backupfile.close()
 
-# Remove the group
+# Remove device from all groups
+for key in inventory_dict['all']['children']:
+    print(key)
+    # If less than 2 devices exist in group and is device to be removed, add dummy
+    device_count = len(inventory_dict['all']['children'][key]['hosts'].keys())
+    # If there's only one key
+    if device_count < 2:
+        # Check if it's the same as the key we're removing.
+        # print(list(inventory_dict['all']['children'][key]['hosts'].keys())[0])
+        if list(inventory_dict['all']['children'][key]['hosts'].keys())[0] == inventory_device:
+            # Add a dummy key
+            inventory_dict['all']['children'][key]['hosts'].update({'dummy' : None})
+
+    # Remove device from group
+    try: 
+        del inventory_dict['all']['children'][key]['hosts'][inventory_device]
+    except Exception:
+        pass
+    
+# Remove device from inventory
 try: 
-    del inventory_dict['all']['children'][inventory_group]
+    del inventory_dict['all']['hosts'][inventory_device]
 except Exception:
     pass
 
 # Pretty print new inventory
-print(">> Pretty print new inventory <<")
-print(yaml.dump(inventory_dict, default_flow_style=False))
+# print(">> Pretty print new inventory <<")
+# print(yaml.dump(inventory_dict, default_flow_style=False))
 
 # Overwrite inventory file
 outfile = open(inventory_file,'w')
